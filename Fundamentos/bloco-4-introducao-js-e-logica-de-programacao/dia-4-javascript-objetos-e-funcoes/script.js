4064b3c61f2905ccb26ebf1b8295cdd47f9e5eca
Sabrina <stx.sabrina@gmail.com>
2021-11-10 18:50:18 -0300

    exercicio 5

--- Fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ Fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -7,6 +7,18 @@ let info = {
  
   info['recorrente'] = 'Sim';
 
-for (let key in info){
-    console.log(info[key]);
-}
+  let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim"
+  };
+
+  let info3 = {
+   perso2: info.personagem + " " + "e" + " " + info2.personagem,
+   ori2: info.origem + " " + "e" + " " + info2.origem,
+   nota2: info.nota + " " + "e" + " " + info2.nota,
+   reco2: "Ambos recorrentes"
+  };
+
+  console.log(info3)
