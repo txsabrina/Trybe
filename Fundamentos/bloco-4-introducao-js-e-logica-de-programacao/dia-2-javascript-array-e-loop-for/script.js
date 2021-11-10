c8b583689a8c07c3868ae704cc1438e5ecf9e817
Sabrina <stx.sabrina@gmail.com>
2021-11-10 11:57:02 -0300

    exercicio 6

--- Fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ Fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,29 +1,20 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 
-//exercício 1
+// Exercicio 6
 
-//for(let index = 0; index <= numbers.length ; index +=1){
-//  console.log(numbers[index])
-//}
+let impar = 0;
 
 
-//Exercício 2 
-let sum = 0;
 
-for(let index = 0; index < numbers.length ; index +=1){
-    sum += numbers[index];
+for(index = 0; index <= numbers.length; index += 1){
+    if (numbers[index] % 2 !== 0){
+        impar += 1;
+    }
+
 }
-
-//console.log(sum);
-//Exercicio 3
-
-let Total = sum/10;
-//console.log(Total);
-//Exercicio 4
-
-if ( Total > 20 ){
-    console.log("valor maior que 20!")
+if(impar === 0){
+    console.log("nenhum valor ímpar encontrado");
 } else {
-    console.log("Valor menor ou igual a 20!")
+    console.log (impar);
 }
