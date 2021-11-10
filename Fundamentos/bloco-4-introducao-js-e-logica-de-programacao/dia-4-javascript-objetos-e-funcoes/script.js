a36bc8b4a07539051fa30cbf0821a0cca6102afe
Sabrina <stx.sabrina@gmail.com>
2021-11-10 18:32:33 -0300

    exercicio 3

--- Fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ Fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -4,8 +4,9 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
-  console.log('Bem-vinda, ' + info.personagem);
-
+ 
   info['recorrente'] = 'Sim';
 
-  console.log(info);
+for (let key in info){
+    console.log(key)
+}
