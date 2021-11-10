bb3dc6d02a1dc738d2c9cd0dc5ba76fb74160080
Sabrina <stx.sabrina@gmail.com>
2021-11-10 19:09:32 -0300

    exercicio 6

--- Fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ Fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -1,24 +1,14 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
   };
 
- 
-  info['recorrente'] = 'Sim';
-
-  let info2 = {
-    personagem: "Tio Patinhas",
-    origem: "Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: "O último MacPatinhas",
-    recorrente: "Sim"
-  };
-
-  let info3 = {
-   perso2: info.personagem + " " + "e" + " " + info2.personagem,
-   ori2: info.origem + " " + "e" + " " + info2.origem,
-   nota2: info.nota + " " + "e" + " " + info2.nota,
-   reco2: "Ambos recorrentes"
-  };
-
-  console.log(info3)
+  console.log( "o livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + leitor.livrosFavoritos[0].titulo);
